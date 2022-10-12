26c8e12b757d59fe59491415062842a677e92696
Pedro <plyncesilva@gmail.com>
2022-10-12 11:50:44 +0200

    Main page template creation (still looks terrible tho)

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
 	path('login', views.log_in, name='login'),
 	path("register", views.register_request, name="register"),
 	path("accounts/", include("django.contrib.auth.urls")),
+	path("main", views.main, name="main"),
+	path("profile", views.profile, name="profile"),
+	path("my_stories", views.my_stories, name="my_stories"),
+	path("new_story", views.new_story, name="new_story"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -25,3 +25,20 @@ def register_request(request):
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="registration/register.html", context={"register_form":form})
+
+def main(request):
+	template = loader.get_template("main_page.html")
+	return HttpResponse(template.render())
+
+def profile(request):
+	template = loader.get_template("profile.html")
+	return HttpResponse(template.render())
+
+
+def new_story(request):
+	template = loader.get_template("new_story.html")
+	return HttpResponse(template.render())
+
+def my_stories(request):
+	template = loader.get_template("my_stories.html")
+	return HttpResponse(template.render())
