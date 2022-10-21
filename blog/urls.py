ae68004463a5e742ef99702d5b8a18051acaa3d1
lourdesbruna <lourdes.bruna@estudiantat.upc.com>
2022-10-21 19:59:43 +0200

    login and register modified to visualize them correctly

--- blog/urls.py
+++ blog/urls.py
@@ -6,10 +6,9 @@ from django.views.generic import RedirectView
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path("", views.home, name="home"),
-	path("<str:name>", views.profile, name="profile"),
-	path("login", views.log_in, name="login"),
-	path("register", views.register_request, name="register"),
-	path("accounts/", include("django.contrib.auth.urls")),
+	path("<int:id>", views.profile, name="profile"),
+	path("register/", views.register_request, name="register"),
+	path("", include("django.contrib.auth.urls")),
 	path("my_stories", views.my_stories, name="my_stories"),
 	path("new_story", views.new_story, name="new_story"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.views.generic import RedirectView
 from .forms import NewUserForm
 from .models import ToDoList, Item
+from .forms import NewUserForm
 
-def profile(request, name):
-	ls = ToDoList.objects.get(name=name) 
-	return HttpResponse("<h1>%s</h1>"%ls.name)
+def profile(request, id):
+	ls = ToDoList.objects.get(id=id) 
+	return HttpResponse("<h1>%s</h1>"%id)
 
 def log_in(request):
 	template = loader.get_template('registration/login.html')
@@ -19,13 +20,15 @@ def register_request(request):
 	if request.method == "POST":
 		form = NewUserForm(request.POST)
 		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render(request=request, template_name="registration/register.html", context={"register_form":form})
+			form.save()
+			#login(request, user)
+			#messages.success(request, "Registration successful." )
+			#return render(request, "blog/main_page.html", {})
+			#messages.error(request, "Unsuccessful registration. Invalid information.")
+		return render(request, "blog/main_page.html", {}) #return redirect("registration//login")
+	else:
+		form = NewUserForm()
+	return render(request, "register/register.html",{"form": form})
 
 def home(request):
 	template = loader.get_template("main_page.html")
