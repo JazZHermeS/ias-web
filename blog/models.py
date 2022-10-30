8f0988199a794e509e452c8a02db8bd5c1cb372e
lourdesbruna <lourdes.bruna@estudiantat.upc.com>
2022-10-30 08:53:31 +0100

    stories+users

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 '''class ToDoList(models.Model):
         name = models.CharField(max_length = 200)
@@ -17,7 +18,7 @@ class Item(models.Model):
 class story(models.Model):
 	title = models.CharField('Story name',max_length=100) 
 	text = models.TextField()
-	#user = models.ManyToManyField(Users)
+	user = models.ManyToManyField(User)
 	
 	def __str__(self):
 		return self.title
